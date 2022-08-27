b418cee3402c372a164cf40e2631a518a68ffb74
karenpamelauxui <karen.pam99@gmail.com>
2022-08-27 17:29:18 +0200

    chore: script modified

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const hamburguer = document.querySelector('.hamburguer');
-const navMenu = document.querySelector('.nav__menu');
+// SELECTORS
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav__menu");
 
-hamburguer.addEventListener('click', () => {
-  hamburguer.classList.toggle('active');
-  navMenu.classList.toggle('active');
+// MOBILE MENU
+hamburguer.addEventListener("click", () => {
+  hamburguer.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll('.nav__link').forEach(link => {
-  link.addEventListener('click', () => {
-    hamburguer.classList.remove('active');
-    navMenu.classList.remove('active');
+document.querySelectorAll(".nav__link").forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburguer.classList.remove("active");
+    navMenu.classList.remove("active");
   });
 });
